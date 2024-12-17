03715fda0de27b5a0a2a1e664f6e8eb140eb2d66
FoshaTyan <nemzik12@gmail.com>
2024-12-17 18:50:34 +0300

    add

--- Trees2/obj/Debug/net8.0/Trees2.AssemblyInfo.cs
+++ Trees2/obj/Debug/net8.0/Trees2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trees2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c24200de065472b34bf6e058a6902b17604156")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trees2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trees2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
